6e6c2c23848504911c12492ba1278faec9e4592a
samieodukunle-dev <samieodukunle@gmail.com>
2025-11-08 12:29:12 -0800

    Add files via upload

---
+++ login page/indxe.js
@@ -0,0 +1,5 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert(" Your login form submitted!");
+});
+console.log("Login page script loaded.");
